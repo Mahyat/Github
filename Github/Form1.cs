4ffcdf66c49a182bf1da2237b305730d1b6a1b0e
Mahya Taghivand <mahya.taghivand@learnet.se>
2020-10-01 12:02:55 +0200

    Add

--- Github/Form1.cs
+++ Github/Form1.cs
@@ -16,5 +16,11 @@ namespace Github
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 10;
+            i = 12;
+        }
     }
 }
